950867dabe0159fe48636e4a9d2729b55695386b
Hüseyin Kaya <kayahuseyin15@gmail.com>
2021-03-02 14:10:52 +0300

    second lesson/constraint layout

---
+++ Bootcamp/app/src/main/java/com/example/bootcamp/ConstraintLayoutProject.kt
@@ -0,0 +1,26 @@
+package com.example.bootcamp
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+
+class ConstraintLayoutProject : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout_project)
+
+        val textInputEditTexUsername: TextInputEditText =
+            findViewById(R.id.textInputEditTextUsername)
+        val textInputEditTextPassword: TextInputEditText =
+            findViewById(R.id.textInputEditTextPassword)
+        val buttonLogin: MaterialButton = findViewById(R.id.buttonLogin)
+
+        buttonLogin.setOnClickListener{
+            Toast.makeText(this,"${textInputEditTexUsername.text}- ${textInputEditTextPassword.text}",Toast.LENGTH_LONG).show()
+        }
+    }
+}
